bcedee900726f89ceeafa0c463105476cb672cf2
LeiDengDengDeng <592737616@qq.com>
2015-10-24 10:30:19 +0800

    车辆信息和常量PO

---
+++ ExpressSystem/src/po/ConstantPO.java
@@ -0,0 +1,37 @@
+package po;
+
+import java.io.Serializable;
+
+public class ConstantPO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7774935977364748669L;
+	
+	double distant;
+	double price;
+	
+	public ConstantPO(double distant, double price) {
+		super();
+		this.distant = distant;
+		this.price = price;
+	}
+
+	public double getDistant() {
+		return distant;
+	}
+
+	public void setDistant(double distant) {
+		this.distant = distant;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
--- ExpressSystem/src/po/TruckPO.java
+++ ExpressSystem/src/po/TruckPO.java
@@ -1,7 +1,27 @@
 package po;
 
 public class TruckPO {
-	
+
 	long number; // 车辆代号
+	int activeTime; // 服役时间
+	String licensePlateNum; // 车牌号
+
+	public TruckPO(long number, int activeTime, String licensePlateNum) {
+		super();
+		this.number = number;
+		this.activeTime = activeTime;
+		this.licensePlateNum = licensePlateNum;
+	}
+	
+	public long getNumber() {
+		return number;
+	}
+	public int getActiveTime() {
+		return activeTime;
+	}
+	public String getLicensePlateNum() {
+		return licensePlateNum;
+	}
+	
 
 }
