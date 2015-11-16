d685423965c2cacfb635506d3570cbcc0440460d
LeeLeeLeeLeeLynn <470549092@qq.com>
2015-11-16 11:53:58 +0800

    Mock更改

--- ExpressSystem/client/src/businesslogic/accountbl/MockAccount.java
+++ ExpressSystem/client/src/businesslogic/accountbl/MockAccount.java
@@ -1,6 +1,9 @@
 package src.businesslogic.accountbl;
+import java.util.ArrayList;
+
 import src.businesslogic.accountbl.Account;
 import src.businesslogicservice.logblservice.LogBLService;
+import src.vo.AccountVO;
 
 public class MockAccount extends Account {
 
@@ -8,8 +11,13 @@ public class MockAccount extends Account {
 		super(log);
 		// TODO Auto-generated constructor stub
 	}
+	public ArrayList<AccountVO> getAccountList() {
+		AccountVO vo1=new AccountVO("Ð¡Íõ×Ó", 1234123412341234234L, 20000);
+		ArrayList<AccountVO> voList=new ArrayList<AccountVO>();
+		return voList;
+	}
 	
-	public void updateAmount(){
-	System.out.println("Ammount Updated!!!");	
+	public void updateAmount(long ID,double Amount){
+	System.out.println(ID+"¡®s ammount Updated!!!: "+Amount);	
 	}
 }
