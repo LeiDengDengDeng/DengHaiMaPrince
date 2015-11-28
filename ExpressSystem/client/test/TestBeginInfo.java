134cdc2d51463b8257dd2405b119dab38e89ac2e
LeeLeeLeeLeeLynn <470549092@qq.com>
2015-11-28 14:48:38 +0800

    注了点

--- ExpressSystem/client/test/TestBeginInfo.java
+++ ExpressSystem/client/test/TestBeginInfo.java
@@ -38,11 +38,11 @@ public class TestBeginInfo {
 		MockLog log = new MockLog(logs);
 		MockAccount account = new MockAccount(log);
 		MockUser user=new MockUser(null,null);
-		MockCommodity commodity=new MockCommodity(log);
+//		MockCommodity commodity=new MockCommodity(log);
 		MockNonUser nonuser=new MockNonUser(log);
 		MockSheet sheet=new MockSheet(null);
 		
-		BeginInfo beginInfo=new BeginInfo(log, account, user, nonuser, commodity, sheet);
+//		BeginInfo beginInfo=new BeginInfo(log, account, user, nonuser, commodity, sheet);
 		ArrayList<DriverInfoVO> beginDriver=new ArrayList<DriverInfoVO>();
 		beginDriver.add(new DriverInfoVO(025001001L, "曹大爷", 1998, 01, 01, "320521199801013421", "13492302391", Sex.FEMALE, 2015));
 		ArrayList<TruckInfoVO> beginTruck=new ArrayList<TruckInfoVO>();
@@ -50,13 +50,13 @@ public class TestBeginInfo {
 		ArrayList<AccountVO> beginAccount=new ArrayList<AccountVO>();
 		beginAccount.add(new AccountVO("小王子",2303240520304205321L , 20000));
 		ArrayList<StorageInitVO> beginStorage=new ArrayList<StorageInitVO>();
-		beginStorage.add(new StorageInitVO(0.9, 1000, 2000, 300, 2000));
+//		beginStorage.add(new StorageInitVO(0.9, 1000, 2000, 300, 2000));
 		ArrayList<ConstantVO> beginConstant=new ArrayList<ConstantVO>();
 		beginConstant.add(new ConstantVO(79, 3, "南京-北京"));
 		ArrayList<InitUserVO>  beginUser=new ArrayList<InitUserVO>();
 		beginUser.add(new InitUserVO(000001L, "总经理", 10000, "小王子"));
 		BeginInfoVO vo=new BeginInfoVO(beginDriver, beginTruck, beginAccount, beginConstant, beginStorage, beginUser);
-		beginInfo.fillInfo(vo);
+//		beginInfo.fillInfo(vo);
 		
 	}
 
