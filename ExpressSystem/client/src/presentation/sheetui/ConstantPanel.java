c2234ec270fd9c5eaab8dd44e80f857f3c7240d7
LeeLeeLeeLeeLynn <470549092@qq.com>
2015-12-23 19:36:04 +0800

    同步

--- ExpressSystem/client/src/presentation/sheetui/ConstantPanel.java
+++ ExpressSystem/client/src/presentation/sheetui/ConstantPanel.java
@@ -1,26 +1,144 @@
 package src.presentation.sheetui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ConstantPanel extends JPanel {
 
 	static final ImageIcon IMG_REC = new ImageIcon("images/constant_rec1.png");
+	static final ImageIcon IMG_REC2 = new ImageIcon("images/account_rec2.png");
 	static final ImageIcon IMG_BG = new ImageIcon("images/constant_bg.png");
+	static final ImageIcon IMG_ButtonConfirm=new ImageIcon("images/account_confirm.png");
 	protected static final int x = 195;// panel 位置x
 	protected static final int y = 70;// panel 位置y
 	protected static final int w = 641;// panel宽
 	protected static final int h = 572;// panel高
+	protected static final int font = 14;
+	Font myFont = new Font("微软雅黑", Font.LAYOUT_NO_LIMIT_CONTEXT, font);
 
 	public ConstantPanel() {
 		this.setLayout(null);
 		this.setBounds(x, y, w, h);
 		this.setOpaque(false);
+		JButton buttonConfirm=new JButton(IMG_ButtonConfirm);
+		buttonConfirm.addMouseListener(new MouseListener(
+				) {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		buttonConfirm.setBounds(500,500,IMG_ButtonConfirm.getIconWidth(),IMG_ButtonConfirm.getIconHeight());
+		this.add(buttonConfirm);
+		
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(IMG_BG.getImage(), 45, 30, null);
+		stub s = new stub();
+		String[][] str = s.getCityList();
+		ArrayList<String> strlist = new ArrayList<String>();
+		for (int i = 0; i < str.length; i++) {
+			for (int j = i + 1; j < str.length; j++) {
+				strlist.add(str[i][0] + " - " + str[j][0]);
+			}
+		}
+		for (int i = 0; i < strlist.size(); i++) {
+			if (i % 2 == 1)
+				g.drawImage(IMG_REC.getImage(), 48, 90 + IMG_REC.getIconHeight() * i, null);
+			if (i % 2 == 0)
+				g.drawImage(IMG_REC2.getImage(), 48, 90 + IMG_REC.getIconHeight() * i, null);
+		}
+		g.setFont(myFont);
+		g.setColor(Color.white);
+		for (int i = 0; i < strlist.size(); i++) {
+			g.drawString(strlist.get(i), 115, 107 + IMG_REC.getIconHeight() * i);
+
+		}
+
 	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		
+		int WIDTH = 850;
+		int HEIGHT = 646;
+		ImageIcon IMG = new ImageIcon("images/mainFrame.png");
+
+		JFrame AccountFrame = new JFrame();
+		ConstantPanel panel = new ConstantPanel();
+		JPanel panelbg = new JPanel();
+		// 设置标题
+		AccountFrame.setUndecorated(true);
+		AccountFrame.setSize(850, 646);
+		// 不允许用户改变窗口大小
+		AccountFrame.setResizable(false);
+		// 获得屏幕大小
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension screen = toolkit.getScreenSize();
+		// 设置窗体位置
+		int x = (screen.width - WIDTH) >> 1;
+		int y = ((screen.height - HEIGHT) >> 1) - 32;
+
+		AccountFrame.setLocation(x, y);
+		// 设置默认panel
+		panelbg.setLayout(null);
+		JLabel bg = new JLabel(IMG);
+		bg.setBounds(0, 0, WIDTH, HEIGHT);
+
+		panelbg.add(panel);
+		AccountFrame.setContentPane(panelbg);
+		AccountFrame.getContentPane().add(bg);
+		AccountFrame.setVisible(true);
+
+	}
+
+	class stub {
+		String[][] getCityList() {
+			String[][] s = new String[5][1];
+			s[0][0] = "南京";
+			s[1][0] = "北京";
+			s[2][0] = "上海";
+			s[3][0] = "深圳";
+			s[4][0] = "广州";
+			return s;
+		}
 	}
 }
--- ExpressSystemServer/server/src/po/ConstantPO.java
+++ ExpressSystemServer/server/src/po/ConstantPO.java
@@ -5,95 +5,101 @@ import src.enums.SheetType;
 
 public class ConstantPO implements SheetPO {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 7774935977364748669L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 7774935977364748669L;
 
-    double distant;
-    double price;
-    String city;
+	double distant;
+	double price;
+	String cityOne;
+	String cityTwo;
 
-    String builder;
-    String time;
-    long ID;
-    SheetState state = SheetState.NOT_EXAMINED;
-    SheetType type = SheetType.CONSTANT;
+	String builder;
+	String time;
+	long ID;
+	SheetState state = SheetState.PASSED;
+	SheetType type = SheetType.CONSTANT;
 
-    public ConstantPO(String builder, String time, double distant, double price, String city, long ID) {
-        super();
-        this.time = time;
-        this.builder = builder;
-        this.distant = distant;
-        this.price = price;
-        this.city = city;
-        this.ID = ID;
-    }
+	public ConstantPO(String builder, String time, double distant, double price, String cityOne, String cityTwo,
+			long ID) {
+		super();
+		this.time = time;
+		this.builder = builder;
+		this.distant = distant;
+		this.price = price;
+		this.cityOne = cityOne;
+		this.cityTwo = cityTwo;
+		this.ID = ID;
+	}
 
-    public double getDistant() {
-        return distant;
-    }
+	public double getDistant() {
+		return distant;
+	}
 
-    public void setDistant(double distant) {
-        this.distant = distant;
-    }
+	public void setDistant(double distant) {
+		this.distant = distant;
+	}
 
-    public double getPrice() {
-        return price;
-    }
+	public double getPrice() {
+		return price;
+	}
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getCityOne() {
+		return cityOne;
+	}
+	public String getCityTwo() {
+		return cityTwo;
+	}
+	public void setCityOne(String city) {
+		this.cityOne = city;
+	}
+	public void setCityTwo(String city) {
+		this.cityTwo = city;
+	}
+	public void setType(SheetType type) {
+		this.type = type;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public SheetType getType() {
+		// TODO 自动生成的方法存根
+		return type;
+	}
 
-    public void setType(SheetType type) {
-        this.type = type;
-    }
+	@Override
+	public void setID(long ID) {
+		this.ID = ID;
+	}
 
-    public SheetType getType() {
-        // TODO 自动生成的方法存根
-        return type;
-    }
+	public long getID() {
+		// TODO 自动生成的方法存根
+		return ID;
+	}
 
-    @Override
-    public void setID(long ID) {
-        this.ID = ID;
-    }
+	@Override
+	public void setSheetState(SheetState state) {
+		// TODO 自动生成的方法存根
+		this.state = state;
+	}
 
-    public long getID() {
-        // TODO 自动生成的方法存根
-        return ID;
-    }
+	@Override
+	public SheetState getSheetState() {
+		// TODO 自动生成的方法存根
+		return state;
+	}
 
-    @Override
-    public void setSheetState(SheetState state) {
-        // TODO 自动生成的方法存根
-        this.state = state;
-    }
+	@Override
+	public String getBuilder() {
+		return builder;
+	}
 
-    @Override
-    public SheetState getSheetState() {
-        // TODO 自动生成的方法存根
-        return state;
-    }
-
-    @Override
-    public String getBuilder() {
-        return builder;
-    }
-
-    @Override
-    public String getTime() {
-        return time;
-    }
-
+	@Override
+	public String getTime() {
+		return time;
+	}
 
 }
