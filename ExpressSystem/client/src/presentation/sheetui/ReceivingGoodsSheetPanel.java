91839d3b1fb3ce81dcf83ded1d354be36395ad18
LeiDengDengDeng <592737616@qq.com>
2015-12-27 14:32:43 +0800

    Up

--- ExpressSystem/client/src/presentation/sheetui/ReceivingGoodsSheetPanel.java
+++ ExpressSystem/client/src/presentation/sheetui/ReceivingGoodsSheetPanel.java
@@ -1,89 +1,119 @@
 package src.presentation.sheetui;
 
+import src.businesslogic.util.CommonUtil;
 import src.businesslogicservice.sheetblservice.SheetBLService;
+import src.presentation.util.TipDialog;
+import src.vo.SheetVO;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class ReceivingGoodsSheetPanel extends SheetPanel {
-	SubPanel subPanel;
-	
-	JLabel imageLabel;
-	ImageIcon bkgImg;
+    SubPanel subPanel;
 
-	public ReceivingGoodsSheetPanel(SheetBLService receivingGoodsBL) {
-		init();
-	}
+    JTextField name;
+    DateChooserJButton date;
+    JLabel imageLabel;
+    ImageIcon bkgImg;
 
-	private void init() {
-		subPanel = new SubPanel();
-		imageLabel = new JLabel();
-		bkgImg = new ImageIcon("images/sheet_receivingGoods.png");
+    public ReceivingGoodsSheetPanel(SheetBLService receivingGoodsBL) {
+        init();
+    }
 
-		subPanel.setBounds(105, 141, 420, 320);
-		
-		imageLabel.setIcon(bkgImg);
-		imageLabel.setBounds(40, 40, bkgImg.getIconWidth(), bkgImg.getIconHeight());
+    private void init() {
+        subPanel = new SubPanel();
+        imageLabel = new JLabel();
+        bkgImg = new ImageIcon("images/sheet_receivingGoods.png");
 
-		this.setLayout(null);
-		this.setOpaque(false);
-		
-		this.setBounds(200, 50, 665, 601);
-		this.add(subPanel);
-		this.add(imageLabel);
-	}
+        subPanel.setBounds(105, 141, 420, 320);
 
-	@Override
-	public boolean confirm() {
-		// TODO 自动生成的方法存根
-		return false;
-	}
+        imageLabel.setIcon(bkgImg);
+        imageLabel.setBounds(40, 40, bkgImg.getIconWidth(), bkgImg.getIconHeight());
 
-	class SubPanel extends JScrollPane {
-		ArrayList<JTextField> courierNumbers;
-		ArrayList<JComboBox> starts;
-		ArrayList<JComboBox> destinations;
-		ArrayList<JComboBox> states;
+        this.setLayout(null);
+        this.setOpaque(false);
 
-		JPanel panel;
-		GridLayout layout;
+        this.setBounds(200, 50, 665, 601);
+        this.add(subPanel);
+        this.add(imageLabel);
+    }
 
-		public SubPanel() {
-			init();
-		}
+    @Override
+    public boolean confirm() {
+        // TODO 自动生成的方法存根
+        if (subPanel.getContents() == null) {
+            new TipDialog(null, "", true, "快递物流编号格式错误", false);
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-		private void init() {
-			layout = new GridLayout(0, 4, 5, 5);
-			panel = new JPanel();
-			panel.setBackground(Color.BLACK);
+    class SubPanel extends JScrollPane {
+        ArrayList<JTextField> courierNumbers;
+        ArrayList<JComboBox> starts;
+        ArrayList<JComboBox> destinations;
+        ArrayList<JComboBox> states;
 
-			panel.setLayout(layout);
-			for (int i = 0; i < 10; i++)
-				addLine();
+        JPanel panel;
+        GridLayout layout;
 
-			this.setViewportView(panel);
-			this.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
-		}
+        public SubPanel() {
+            courierNumbers = new ArrayList();
+            starts = new ArrayList();
+            destinations = new ArrayList();
+            states = new ArrayList();
+            init();
+        }
 
-		public void addLine() {
-			JTextField courierNumber = new JTextField();
-			JComboBox start = new JComboBox();
-			JComboBox destination =  new JComboBox();
-			JComboBox state = new JComboBox(new String[]{"完整","损坏","丢失"});
+        private void init() {
+            layout = new GridLayout(0, 4, 5, 5);
+            panel = new JPanel();
+            panel.setBackground(Color.BLACK);
 
-			courierNumbers.add(courierNumber);
-			starts.add(start);
-			destinations.add(destination);
-			starts.add(start);
+            panel.setLayout(layout);
+            for (int i = 0; i < 10; i++)
+                addLine();
 
-			panel.add(courierNumber);
-			panel.add(start);
-			panel.add(destination);
-			panel.add(state);
+            this.setViewportView(panel);
+            this.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
+        }
 
-			repaint();
-		}
-		
-	}
+        public void addLine() {
+            JTextField courierNumber = new JTextField();
+            JComboBox start = new JComboBox();
+            JComboBox destination = new JComboBox();
+            JComboBox state = new JComboBox(new String[]{"完整", "损坏", "丢失"});
+
+            courierNumbers.add(courierNumber);
+            starts.add(start);
+            destinations.add(destination);
+            starts.add(start);
+
+            panel.add(courierNumber);
+            panel.add(start);
+            panel.add(destination);
+            panel.add(state);
+
+            repaint();
+        }
+
+        public ArrayList<String[]> getContents() {
+            ArrayList<String[]> res = new ArrayList<>();
+            for (int i = 0; i < courierNumbers.size(); i++) {
+                if (!courierNumbers.get(i).getText().equals("")) {
+                    if (!CommonUtil.isValidNumberString(courierNumbers.get(i).getText(), 10))
+                        return null;
+                    String[] temp = new String[4];
+                    temp[0] = courierNumbers.get(i).getText();
+                    temp[1] = (String) starts.get(i).getSelectedItem();
+                    temp[2] = (String) destinations.get(i).getSelectedItem();
+                    temp[3] = (String) states.get(i).getSelectedItem();
+                }
+            }
+            return res;
+        }
+
+    }
 }
