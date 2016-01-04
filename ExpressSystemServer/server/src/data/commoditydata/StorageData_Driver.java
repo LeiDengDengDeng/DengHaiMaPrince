b0836ca6d4db18605c374b486565d84228782ee6
JImmyPotter <836xhw@sina.com>
2016-01-05 01:31:58 +0800

    ww

--- ExpressSystemServer/server/src/data/commoditydata/StorageData_Driver.java
+++ ExpressSystemServer/server/src/data/commoditydata/StorageData_Driver.java
@@ -13,18 +13,34 @@ public class StorageData_Driver {
 		ArrayList<String> logisticsState = new ArrayList<String>();
 		logisticsState.add("南京到扬州");
 		ArrayList<GoodsPO> gpos = new ArrayList<GoodsPO>();
-		GoodsPO goodsPO1 = new GoodsPO(logisticsState, 3, 3.0, 3.0, 3.0, 
-				3.0, 3.0, "血吼", GoodsType.ECONOMIC, GoodsType.PAPERPAKAGE,
-				"0000000000000002", "2015-12-30", "扬州", GoodsType.RAIL, 103, 103, 103, 
-				001, "0250201510250000000", 
-				"0250201510250000000");
-		GoodsPO goodsPO2 = new GoodsPO(logisticsState, 3, 3.0, 3.0, 3.0, 
+//		GoodsPO goodsPO1 = new GoodsPO(logisticsState, 3, 3.0, 3.0, 3.0, 
+//				3.0, 3.0, "血吼", GoodsType.ECONOMIC, GoodsType.PAPERPAKAGE,
+//				"0000000000000002", "2015-12-30", "扬州", GoodsType.RAIL, 103, 103, 103, 
+//				001, "0250201510250000000", 
+//				"0250201510250000000");
+//		GoodsPO goodsPO2 = new GoodsPO(logisticsState, 3, 3.0, 3.0, 3.0, 
+//				3.0, 3.0, "灰烬使者", GoodsType.ECONOMIC, GoodsType.PAPERPAKAGE,
+//				"0000000000000000", "2015-12-20", "扬州", GoodsType.RAIL, 003, 003, 003, 
+//				001, "0250201510250000000",
+//				"0250201510250000000");
+//		gpos.add(goodsPO1);
+//		gpos.add(goodsPO2);
+		for (int i = 0; i < 28; i++) {
+			GoodsPO goodsPO1 = new GoodsPO(logisticsState, 3, 3.0, 3.0, 3.0, 
+					3.0, 3.0, "血吼", GoodsType.ECONOMIC, GoodsType.PAPERPAKAGE,
+					"0000000000000002", "2015-01-05", "扬州", GoodsType.RAIL, 103, 103, 103, 
+					001, "0250201510250000000", 
+					"0250201510250000000");
+          gpos.add(goodsPO1);
+      }
+  	for (int i = 0; i < 28; i++) {
+  		GoodsPO goodsPO2 = new GoodsPO(logisticsState, 3, 3.0, 3.0, 3.0, 
 				3.0, 3.0, "灰烬使者", GoodsType.ECONOMIC, GoodsType.PAPERPAKAGE,
-				"0000000000000000", "2015-12-20", "扬州", GoodsType.RAIL, 003, 003, 003, 
+				"0000000000000000", "2015-01-05", "扬州", GoodsType.RAIL, 003, 003, 003, 
 				001, "0250201510250000000",
 				"0250201510250000000");
-		gpos.add(goodsPO1);
-		gpos.add(goodsPO2);
+          gpos.add(goodsPO2);
+      }
 		StoragePO spo1 = new StoragePO("南京", 1000, 200, 200, 200, 200, 0.9, 250, 250, 
 				250, 250, gpos);
 		StoragePO spo2 = new StoragePO("上海", 1000, 200, 200, 200, 200, 0.9, 250, 250, 
@@ -36,7 +52,7 @@ public class StorageData_Driver {
 		System.out.println(storageData.finds().size());
 //		System.out.println(storageData.finds().get(0).getCity() + " "
 //				+ storageData.finds().get(1).getCity());
-		System.out.println(storageData.findStoragePO("南京").getAlarmScale());
+		System.out.println(storageData.findStoragePO("上海").getGpos().size());
 		System.out.println("gg" + storageData.findStoragePO("上海").getGpos().get(1).getInTime());
 		System.out.println(storageData.finds().get(0).getCity());
 	}
